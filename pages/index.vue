3d0412ba87f6e27ee1d792208b6d58c5fea938dc
a.nishioka0817 <a.nishioka0817@gmail.com>
2024-06-15 15:30:55 +0900

    sassを追加する

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,101 @@
 <template>
   <h1>index.vue</h1>
   <AtomsModal />
+  <v-container>
+    <v-data-table :headers="headers" :items="items" :items-per-page="5">
+      <template #headers>
+        <tr>
+          <th class="header"></th>
+        </tr>
+      </template>
+    </v-data-table>
+  </v-container>
 </template>
-<script setup lang="ts"></script>
-<style lang="scss" scoped></style>
+<script setup lang="ts">
+const headers = ref([
+  { title: 'Plant', align: 'start', sortable: false, key: 'name' },
+  { title: 'Light', align: 'end', key: 'light' },
+  { title: 'Height', align: 'end', key: 'height' },
+  { title: 'Pet Friendly', align: 'end', key: 'petFriendly' },
+  { title: 'Price ($)', align: 'end', key: 'price' },
+])
+
+const items = ref([
+  {
+    name: 'Fern',
+    light: 'Low',
+    height: '20cm',
+    petFriendly: 'Yes',
+    price: 20,
+  },
+  {
+    name: 'Snake Plant',
+    light: 'Low',
+    height: '50cm',
+    petFriendly: 'No',
+    price: 35,
+  },
+  {
+    name: 'Monstera',
+    light: 'Medium',
+    height: '60cm',
+    petFriendly: 'No',
+    price: 50,
+  },
+  {
+    name: 'Pothos',
+    light: 'Low to medium',
+    height: '40cm',
+    petFriendly: 'Yes',
+    price: 25,
+  },
+  {
+    name: 'ZZ Plant',
+    light: 'Low to medium',
+    height: '90cm',
+    petFriendly: 'Yes',
+    price: 30,
+  },
+  {
+    name: 'Spider Plant',
+    light: 'Bright, indirect',
+    height: '30cm',
+    petFriendly: 'Yes',
+    price: 15,
+  },
+  {
+    name: 'Air Plant',
+    light: 'Bright, indirect',
+    height: '15cm',
+    petFriendly: 'Yes',
+    price: 10,
+  },
+  {
+    name: 'Peperomia',
+    light: 'Bright, indirect',
+    height: '25cm',
+    petFriendly: 'Yes',
+    price: 20,
+  },
+  {
+    name: 'Aloe Vera',
+    light: 'Bright, direct',
+    height: '30cm',
+    petFriendly: 'Yes',
+    price: 15,
+  },
+  {
+    name: 'Jade Plant',
+    light: 'Bright, direct',
+    height: '40cm',
+    petFriendly: 'Yes',
+    price: 25,
+  },
+])
+</script>
+<style lang="scss" scoped>
+.header {
+  background-color: blue;
+  color: white;
+}
+</style>
--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -2,8 +2,18 @@ import "@mdi/font/css/materialdesignicons.css";
 
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 export default defineNuxtPlugin((app) => {
-  const vuetify = createVuetify({});
+  const vuetify = createVuetify({
+    components,
+    directives,
+    theme: {
+      themes: {
+        light: {},
+      },
+    },
+  });
   app.vueApp.use(vuetify);
 });

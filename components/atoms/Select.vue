18919e6540cf9100b71b15e62766076c504f29df
a.nishioka0817 <a.nishioka0817@gmail.com>
2024-06-25 22:06:40 +0900

    select: selectを追加する

---
+++ components/atoms/Select.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-select v-model="selected" :items="items" label="Select an item"
+    :menu-icon="isOpen ? 'mdi-chevron-up' : 'mdi-chevron-down'" @click:menu="toggleMenu">
+    <template v-slot:append-inner>
+      <!-- 空のスロットを追加することでデフォルトのアイコンを隠す -->
+    </template>
+  </v-select>
+</template>
+
+<script setup lang="ts">
+const selected = ref<string | null>(null);
+const items = ref<string[]>(['Item 1', 'Item 2', 'Item 3']);
+const isOpen = ref(false);
+const toggleMenu = () => {
+  isOpen.value = !isOpen.value;
+};
+</script>
+
+<style lang="scss" scoped>
+.custom-v-select .v-input__append-inner {
+  display: none;
+}
+</style>

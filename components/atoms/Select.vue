b02fbafde079f02237363b9d630a8ebd6fc4553b
a.nishioka0817 <a.nishioka0817@gmail.com>
2024-07-15 14:06:02 +0900

    Selectを追加する

--- components/atoms/Select.vue
+++ components/atoms/Select.vue
@@ -1,20 +1,11 @@
 <template>
-  <v-select v-model="selected" :items="items" label="Select an item"
-    :menu-icon="isOpen ? 'mdi-chevron-up' : 'mdi-chevron-down'" @click:menu="toggleMenu">
-    <template v-slot:append-inner>
-      <!-- 空のスロットを追加することでデフォルトのアイコンを隠す -->
-    </template>
+  <v-select label="Select Item" multiple>
+    <slot name="selection"></slot>
   </v-select>
 </template>
 
-<script setup lang="ts">
-const selected = ref<string | null>(null);
-const items = ref<string[]>(['Item 1', 'Item 2', 'Item 3']);
-const isOpen = ref(false);
-const toggleMenu = () => {
-  isOpen.value = !isOpen.value;
-};
-</script>
+<script setup lang=" ts">
+      </script>
 
 <style lang="scss" scoped>
 .custom-v-select .v-input__append-inner {
--- pages/sample/SelectPage.vue
+++ pages/sample/SelectPage.vue
@@ -1,7 +1,19 @@
 <template>
   <h1>sample/SelectPage.vue</h1>
-  <AtomsSelect />
+  <AtomsSelect v-model="value" :items="items">
+    <template #selection="{ item, index }">
+      <v-chip v-if="index < 2">
+        <span>{{ item.title }}</span>
+      </v-chip>
+      <span v-if="index === 2" class="text-grey text-caption align-self-center">
+        (+{{ value.length - 2 }} others)
+      </span>
+    </template>
+  </AtomsSelect>
 </template>
 <script setup lang="ts">
+const items = ['foo', 'bar', 'fizz', 'buzz', 'fizzbuzz', 'foobar']
+
+const value = ref(['foo', 'bar', 'fizz'])
 </script>
 <style lang="scss" scoped></style>

a95eded3910e897c11f6a3eb4cfba20acea111fb
a.nishioka0817 <a.nishioka0817@gmail.com>
2024-07-27 16:50:32 +0900

    checkbox: チェックボックスページの追加

---
+++ components/atoms/CheckBox.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-checkbox color="red" base-color="red" label="red" hide-details></v-checkbox>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/sample/CheckBoxPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>sample/CheckBoxPage</h1>
+  <AtomsCheckBox></AtomsCheckBox>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped></style>

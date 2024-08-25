ddb7f962320592a7a0ba7dccc2116bd176d97f9b
a.nishioka0817 <a.nishioka0817@gmail.com>
2024-08-25 13:52:33 +0900

    chip: sample追加

---
+++ components/atoms/Chip.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-chip :text="text" :variant="variant" density="default" size="default">
+  </v-chip>
+</template>
+
+<script setup lang="ts">
+defineProps
+  ({
+    text: { type: String, default: 'text' },
+    variant: { type: String, default: 'tonal' },
+  });
+
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/sample/ChipPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>sample/ChipPage</h1>
+  <AtomsChip text="sample" variant="tonal"></AtomsChip>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped></style>

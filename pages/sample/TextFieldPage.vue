cc7235b6da470d7e11998887509029ffff62c51e
a.nishioka0817 <a.nishioka0817@gmail.com>
2024-09-29 13:21:33 +0900

    deleteボタンのサンプル追加

--- pages/sample/TextFieldPage.vue
+++ pages/sample/TextFieldPage.vue
@@ -1,8 +1,23 @@
 <template>
   <h1>sample/TextFieldPage.vue</h1>
-  <AtomsTextField v-model="text" />
+  <AtomsTextField v-model="text" label="test" @click:clear="onClear" />
+  <AtomsButton @click="onClick" />
 </template>
 <script setup lang="ts">
-const text = 'test';
+const text = ref('test');
+
+const onClear = () => {
+  // focusがあたっている要素を取得
+  const elem = <HTMLInputElement>document.activeElement;
+  console.log("onClear", elem);
+  if (elem) elem.blur();
+}
+
+const onClick = () => {
+  // focusがあたっている要素を取得
+  const elem = document.activeElement;
+  console.log("onClick", elem);
+  text.value = '';
+}
 </script>
 <style lang="scss" scoped></style>

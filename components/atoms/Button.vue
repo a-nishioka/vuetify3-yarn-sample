be48ffed94048bcf5a84fd96a10359a493cf99ba
a.nishioka0817 <a.nishioka0817@gmail.com>
2025-03-15 21:52:12 +0900

    v-btnのclickイベントを発火する

--- components/atoms/Button.vue
+++ components/atoms/Button.vue
@@ -1,12 +1,24 @@
 <template>
-  <v-btn @click="onClick">Click me</v-btn>
+  <v-btn ref="btnRef" @click="onClick">Click me</v-btn>
 </template>
 
 <script setup lang="ts">
+import type { VBtn } from 'vuetify/components'
+
 const emits = defineEmits<{ (e: 'click'): void }>()
 const onClick = (): void => {
+  console.log('onClick')
   emits('click')
 }
+
+// v-btn の参照を作成
+const btnRef = useTemplateRef<VBtn | null>('btnRef')
+
+onMounted(() => {
+  if (btnRef.value?.$el) {
+    (btnRef.value.$el as HTMLButtonElement).click()
+  }
+})
 </script>
 
 <style lang="scss" scoped></style>
